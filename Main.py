2b431bac331175e060f3ce1ea2b37d9c56bf8711
Abolfazl-felix <abolfazl.b.h.1386.25@gmail.com>
2026-09-19 08:56:04 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,18 +3,25 @@ from persiantools.jdatetime import JalaliDate
 from datetime import date
 import asyncio
 import os
-TOKEN = os.getenv("BOT_TOKEN")
+
+TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = -1004297391995
-bot = Bot(token=TOKEN)
-today = JalaliDate.today()
-birthday = JalaliDate(1405, 9, 30)
-days = (birthday.to_gregorian() - date.today()).days
+
+# تولد: ۳۰ آذر ۱۴۰۵
+birthday = JalaliDate(1405, 9, 30).to_gregorian()
+today = date.today()
+
+days = (birthday - today).days
+
 if days > 0:
     text = f"🎂❤️ تا تولد آناهیتا {days} روز مونده 🎉"
 elif days == 0:
     text = "🎉🎂 امروز تولد آناهیتاست ❤️"
-else
-    text = "🎉 تولد آناهیتا گذشته ❤️"
-async def send():
+else:
+    text = "🎂 تولد آناهیتا گذشته ❤️"
+
+async def main():
+    bot = Bot(TOKEN)
     await bot.send_message(chat_id=CHAT_ID, text=text)
-asyncio.run(send())
+
+asyncio.run(main())
