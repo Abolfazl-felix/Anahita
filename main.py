8c3f74ec337cb6405de271bcb6ed7327eea8be40
Abolfazl-felix <abolfazl.b.h.1386.25@gmail.com>
2026-09-20 13:06:09 +0330

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,13 @@ else:
 
 async def main():
     bot = Bot(TOKEN)
+
     for chat_id in CHAT_IDS:
-    await bot.send_message(chat_id=chat_id, text=text)
+        await bot.send_message(
+            chat_id=chat_id,
+            text=text
+        )
+
+asyncio.run(main())
 
 asyncio.run(main())
